84bc22285a9fea3ea9451218b89008a3c63dfe35
Umar <muhammadshafqatali3@gmail.com>
2023-08-24 16:28:27 +0500

    dsdsd

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 let timer = 60;
-let hits = 0;
+let newValue;
 
+function addEventListenerToCircles() {
+  let circleBtns = document.querySelectorAll("#cirkle");
+  circleBtns.forEach((circleBtn) => {
+    circleBtn.addEventListener("click", () => {
+      console.log(newValue);
+    });
+  });
+}
 const makeBubble = () => {
   let circles = "";
   for (let i = 0; i <= 120; i++) {
-    circles += `<div class="bg-sky-600 w-11 h-11 rounded-full relative hover:bg-sky-700 cursor-pointer">
+    circles += `<div id="cirkle" class="bg-sky-600 w-11 h-11 rounded-full relative hover:bg-sky-700 cursor-pointer">
                     <p class="text-white font-semibold mt-[10px] text-center  ">
                     ${Math.floor(Math.random() * 9)}
                     </p>
                 </div>`;
   }
   document.querySelector("#circle-container").innerHTML = circles;
+  addEventListenerToCircles();
 };
 
 const runTimer = () => {
@@ -25,9 +34,8 @@ const runTimer = () => {
 };
 
 const newValueToGuess = () => {
-  document.querySelector("#hits-val").textContent = Math.floor(
-    Math.random() * 9
-  );
+  newValue = Math.floor(Math.random() * 9);
+  document.querySelector("#hits-val").textContent = newValue;
 };
 
 newValueToGuess();
